4bba6e367c43a7781f4c9d2a2047ff1dc9cb9c37
Kolbe Kegel <kolbe@kolbekegel.com>
2014-05-28 01:31:00 +0200

    added parallel-load.bash

---
+++ parallel-load.bash
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+# Usage: parallel-load <numthreads> <table name> <filename>
+
+# this script takes a number of threads, a table name, and a filename
+# it creates FIFOs, determines the size of the file, splits it up into roughly-even chunks,
+# and sends one chunk to each FIFO. a number of mysql clients are spawned and a separate
+# LOAD DATA INFILE command is executed in each to read from the FIFOs
+
+# put mysql login information in a ~/.my.cnf file or other location where this script can read it
+
+threads=$1
+shift
+
+table=$1
+shift
+
+fifodir="/tmp"
+
+files=("$@")
+offsets=(1)
+fifos=()
+
+if ! lock_mode=$(mysql -BNe 'select @@innodb_autoinc_lock_mode'); then
+    echo "[ERROR] couldn't get server information from MySQL. Aborting."
+    exit 1
+fi
+
+if ! has_auto_inc=$(mysql -BNe "select count(*) from information_schema.columns where concat(table_schema,'.',table_name) in (concat(database(),'.','$table'),'$table')  and extra like '%auto_increment%'"); then
+    echo "[ERROR] couldn't get table information from MySQL. Aborting."
+    exit 1
+fi
+
+if ((has_auto_inc && lock_mode != 2)); then
+    echo "[ERROR] $table has an auto-inc column and innodb_autoinc_lock_mode is not 2 (\"interleaved\"). Parallel threads will be blocked." >&2
+    exit 2
+fi
+
+
+for ((t=0;t<threads;t++)); do
+    fifos+=("$fifodir/parallel_load_$$_$t.fifo")
+    if ! mkfifo "${fifos[t]}"; then
+        echo "[ERROR] failed to create fifo #$t: $f. Aborting." >&2
+        exit 3
+    fi
+done
+
+for f in "${files[@]}"; do 
+    read -r size _ < <(wc -c "$f")
+    chunk_size=$((size / threads))
+    printf "%i bytes total\n%i bytes per chunk\n%i threads\n" "$size" "$chunk_size" "$threads"
+    for ((t=0;t<threads;t++)); do
+        guess=$((offsets[t] + chunk_size))
+        extra=$(tail -c +"$guess" "$f" | head -n 1 | wc -c)
+        offsets+=($((guess + extra)))
+    done
+
+    for ((o=0;o<"${#offsets[@]}"-1;o++)); do
+        start=${offsets[o]}
+        bytes=$((${offsets[o+1]} - start))
+        fifo=${fifos[o]}
+        tail -c +"$start" "$f" | head -c "$bytes" >> "$fifo" &
+        printf "Thread #%i reading from %i for %i bytes in PID %i" "$o" "$start" "$bytes" "$!"
+        mysql -e "LOAD DATA INFILE '$fifo' INTO TABLE $table" &
+        printf " executed by mysql client PID %i\n" "$!"
+        last_client=$!
+    done
+
+done
+
+for fifo in "${fifos[@]}"; do
+    if ! rm "$fifo"; then
+        echo "[WARNING] failed to remove fifo: $fifo" >&2
+    fi
+done
+
+# echo "Waiting for PID $last_client to finish..." && wait $last_client
